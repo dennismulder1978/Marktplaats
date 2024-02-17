f3e28b9e7449d6a92b27e98edd9b0dce2e860f88
Dennis <dnns.mldr@gmail.com>
2024-02-17 10:38:00 +0100

    tewst

--- start.py
+++ start.py
@@ -1,1 +1,35 @@
-print('martkplaats')
+from marktplaats import SearchQuery
+
+search = SearchQuery("drumstel", # Search query
+                     zip_code="1341DC", # Zip code to base distance from
+                     distance=0, # Max distance (meters) from the zip code for listings
+                     price_from=0, # Lowest price to search for
+                     price_to=100, # Highest price to search for
+                     limit=5, # Max listings (page size, max 25)
+                     offset=0) # Offset for listings (page * limit)
+
+listings = search.get_listings()
+
+for listing in listings:
+    print(listing.title)
+    # print(listing.description)
+    print(listing.price)
+    # print(listing.link)
+    
+    # # the location object
+    # print(listing.location)
+    
+    # # the seller object
+    # print(listing.seller)
+    
+    # # the datetime object
+    # print(listing.date)
+    
+    # # the full seller object (another request)
+    # print(listing.seller.get_seller())
+    
+    # for image in listing.images:
+    #     print(image.medium)
+    
+    
+    print("-----------------------------")
